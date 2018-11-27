50cab99e7614dce40250dfb14a0f2218d4bbc49b
Jason Stock <stock@earth.cs.colostate.edu>
2018-11-26 20:13:52 -0700

    Final Changes

--- src/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm *.class ||: \
-&& /usr/local/hadoop/bin/hadoop com.sun.tools.javac.Main ./WordCount.java \
-&& jar cf WordCount.jar WordCount*.class \
-&& /usr/local/hadoop/bin/hadoop fs -rm -R /tmp/out ||: \
-&& /usr/local/hadoop/bin/hadoop jar ./WordCount.jar WordCount /tmp/data /tmp/out
-#&& /usr/local/hadoop/bin/hadoop fs -cat /tmp/out/*
